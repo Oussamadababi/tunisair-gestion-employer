73b1d78fdb7ecc2be7d0032fdcf52eb1d7326312
Dababi Oussama <oussama.dababi@esprit.tn>
2021-05-21 23:31:55 +0200

    Get Role by id user

--- src/main/java/com/tunisair/dao/userEntityRepository.java
+++ src/main/java/com/tunisair/dao/userEntityRepository.java
@@ -14,6 +14,12 @@ public interface userEntityRepository extends JpaRepository<UserEntity, String> 
 	@Query(value = "SELECT name FROM keycloak_role where name!='create-client' and name!='offline_access' and name!='view-applications' and name!='admin'and name!='create-realm' and name!='create-client' and name!='view-realm' and name!='create-client' and name!='view-users' and name!='view-clients' and name!='view-realm' and name!='create-client' and name!='view-events' and name!='view-identity-providers' and name!='view-authorization' and name!='manage-realm' and name!='manage-users' and name!='manage-clients' and name!='manage-events' and name!='manage-identity-providers' and name!='manage-authorization' and name!='query-users' and name!='query-clients' and name!='query-realms' and name!='query-groups' and name!='view-profile' and name!='manage-account' and name!='manage-account-links' and name!='view-consent' and name!='manage-consent' and name!='delete-account' and name!='read-token' and name!='impersonation' and name!='uma_authorization' and name!='realm-admin' and name!='impersonation' and name!='uma_authorization'", nativeQuery = true)
 	public  List<String> ListeDesNouveauxRoles();
 	
+	@Query(value = "SELECT role_id,name FROM user_role_mapping INNER JOIN keycloak_role ON user_role_mapping.role_id = keycloak_role.id where user_id=?1 and name!='create-client' and name!='offline_access' and name!='view-applications' and name!='admin'and name!='create-realm' and name!='create-client' and name!='view-realm' and name!='create-client' and name!='view-users' and name!='view-clients' and name!='view-realm' and name!='create-client' and name!='view-events' and name!='view-identity-providers' and name!='view-authorization' and name!='manage-realm' and name!='manage-users' and name!='manage-clients' and name!='manage-events' and name!='manage-identity-providers' and name!='manage-authorization' and name!='query-users' and name!='query-clients' and name!='query-realms' and name!='query-groups' and name!='view-profile' and name!='manage-account' and name!='manage-account-links' and name!='view-consent' and name!='manage-consent' and name!='delete-account' and name!='read-token' and name!='impersonation' and name!='uma_authorization' and name!='realm-admin' and name!='impersonation' and name!='uma_authorization'", nativeQuery = true)
+	public  List<Object> GetRoleByIdUser(String Iduser);
+	
+	
+	
+	
 	
 	
 	
