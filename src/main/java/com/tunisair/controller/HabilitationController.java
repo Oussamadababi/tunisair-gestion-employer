905097adbb8ae3bf42101b7732f6a75435a18ceb
Dababi Oussama <oussama.dababi@esprit.tn>
2021-06-07 13:36:56 +0200

    Habilitaton crud

--- src/main/java/com/tunisair/controller/HabilitationController.java
+++ src/main/java/com/tunisair/controller/HabilitationController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,10 @@ public class HabilitationController {
 	public List <Habilitation> findall(){	
 		return  Hs.findall();
 		}
-
+	@DeleteMapping(value="/delete/{id}")
+	public void delete(@PathVariable(name="id") Long id){
+		Hs.delete(id);
+		
+	}
 
 }
